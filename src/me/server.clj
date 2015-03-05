a0654d22b1fba8bba4e254e76693e794bab0a6a2
Junior Brito <Jjunior130@gmail.com>
2015-03-04 22:27:20 -0500

    commits

--- src/me/server.clj
+++ src/me/server.clj
@@ -2,5 +2,7 @@
   (:require [me.handler :refer :all]
         [ring.adapter.jetty :as jetty]))
 
-(defn -main [port]
-  (jetty/run-jetty app {:port (if port (Integer. port) 8000)}))
+(defn -main
+  ([port] (jetty/run-jetty app {:port (Integer. port)}))
+  ([] (-main 8000)))
+
